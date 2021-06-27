769324dfa626f3331be3464b489348f1e9623083
MaazMakrod <67477587+MaazMakrod@users.noreply.github.com>
2021-06-27 11:44:12 -0400

    Add JS File

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector(".github-repo").addEventListener('click', function(e) {
+  location.replace("https://github.com/MaazMakrod/Discord-Earthquake-Bot");
+});
+
+
+document.querySelector(".video-link").addEventListener('click', function(){
+  location.replace("https://drive.google.com/file/d/1_Dn9jH1fRRQt5-uhoIQJqHneNkyU95XC/view?usp=sharing");
+})
